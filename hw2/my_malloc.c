45a935d30b984cf775c0d735f5deb49d86f824ee
Camron Fritz <camfritz@Camrons-MacBook-Pro.local>
2018-01-24 22:52:11 -0800

    1-24-18 22:52

--- hw2/my_malloc.c
+++ hw2/my_malloc.c
@@ -3,14 +3,15 @@
 free_list_node *head = NULL;
 
 void *my_malloc(int size) {
-	printf("my_malloc: called with size: %d\n", size);
+	fprintf(stderr, "my_malloc: called with size: %d\n", size);
 	//some error checking on sizes
 	if(size < 0) {
 		fprintf(stderr, "Usage error: Size must be non-negative\n");
 		return NULL;
 	}
-	if(size > 2048) {
-		fprintf(stderr, "Usage error: Size cannot be greater than SIZE constant 2048\n");
+	if(size > 2032) {
+		fprintf(stderr, "Usage error: Size cannot be greater than 2032\n");
+		return NULL;
 	}
 	if(SIZE > 2048) {
 		fprintf(stderr, "Header Error: SIZE constant greater than 2048\n");
@@ -23,8 +24,12 @@ void *my_malloc(int size) {
 	void *mem;
 	//point head to initial free list
 	if(head == NULL) {
-		printf("Head Node is NULL, expanding heap\n");
-		current = (free_list_node *) sbrk(SIZE);
+		fprintf(stderr, "Head Node is NULL, expanding heap\n");
+		current = (free_list_node *) sbrk(SIZE);	//error check sbrk()
+		if(current == -1 || current == NULL) {
+			fprintf(stderr, "ERROR: bad sbrk()\n");
+			exit(1);
+		}
 		current->size = SIZE - sizeof(free_list_node);
 		current->next = NULL;
 		head = current;
@@ -39,7 +44,11 @@ void *my_malloc(int size) {
 				mem = current + sizeof(free_list_node);
 				current = current->next;
 				previous->next = current;
-				printf("Scanning free list...space found in free list\n");
+				fprintf(stderr, "Scanning free list...space found in free list\n");
+				if(mem == NULL) {
+					fprintf(stderr, "error: bad pointer\n");
+					exit(1);
+				}
 				return mem;
 			}
 			else {
@@ -47,7 +56,11 @@ void *my_malloc(int size) {
 				mem = current + sizeof(free_list_node);
 				current = current->next;
 				head = current;
-				printf("Scanning free list...space found in free list\n");
+				fprintf(stderr, "Scanning free list...space found in free list\n");
+				if(mem == NULL) {
+					fprintf(stderr, "error: bad pointer\n");
+					exit(1);
+				}
 				return mem;
 			}
 		}
@@ -55,24 +68,32 @@ void *my_malloc(int size) {
 
 			if(current != head) {
 				mem = current + sizeof(free_list_node);
-				newNode = current + sizeof(free_list_node) + size;
+				newNode = ((free_list_node*) current + sizeof(free_list_node) + size);
 				current->size = current->size - size - sizeof(free_list_node);
 				newNode->next = current->next;
 				newNode->size = current->size;
 				current->size = size;
 				previous->next = newNode;
-				printf("Scanning free list...space found in free list\n");
+				fprintf(stderr, "Scanning free list...space found in free list\n");
+				if(mem == NULL) {
+					fprintf(stderr, "error: bad pointer\n");
+					exit(1);
+				}
 				return mem;
 			}
 			else {
 				mem = current + sizeof(free_list_node);
-				newNode = current + sizeof(free_list_node) + size;
-				current->size = current->size - size - sizeof(free_list_node);
+				newNode = ((free_list_node*) (current + sizeof(free_list_node) + size));
+				current->size = (unsigned int) current->size - size - sizeof(free_list_node);
 				newNode->next = current->next;
-				newNode->size = current->size;
+				newNode->size = (unsigned int) current->size;
 				current->size = size;
 				head = newNode;
-				printf("Scanning free list...space found in free list\n");
+				fprintf(stderr, "Scanning free list...space found in free list\n");
+				if(mem == NULL) {
+					fprintf(stderr, "error: bad pointer\n");
+					exit(1);
+				}
 				return mem;
 			}
 		}
@@ -81,14 +102,22 @@ void *my_malloc(int size) {
 	} while(current != NULL);
 
 	//expand heap if no memory available for allocation
-	printf("Scanning free list...no space in free list\n");
-	printf("Expanding heap with sbrk(), recursively calling my_malloc()\n");
+	fprintf(stderr, "Scanning free list...no space in free list\n");
+	fprintf(stderr, "Expanding heap with sbrk(), recursively calling my_malloc()\n");
 	newNode = (free_list_node *) sbrk(SIZE);
+	if(newNode == -1 || newNode == NULL) {	//error check sbrk()
+		fprintf(stderr, "ERROR: bad sbrk()\n");
+		exit(1);
+	}
 	current = newNode;
 	current->size = SIZE - 16;
 	current->next = NULL;
 	previous->next = current;
 	mem = (void *) my_malloc(size);
+	if(mem == NULL) {
+		fprintf(stderr, "error: bad pointer\n");
+		exit(1);
+	}
 	return mem;
 
 }
@@ -113,7 +142,7 @@ void my_free(void *ptr) {
 	//append freed memory to free list
 	current->next = freeNode;
 	freeNode->next = NULL;
-	printf("my_free: next = 0x%x\n", (unsigned int) freeNode->next);
+	fprintf(stderr, "my_free: next = 0x%x\n", (unsigned int) freeNode->next);
 }
 
 void print_free_list() {
@@ -121,8 +150,8 @@ void print_free_list() {
 	int nd, addr, sz, nx;
 	free_list_node *current = head;
 
-	printf("===============FREE LIST==============\n");
-	printf("NODE # |   ADDRESS |   SIZE |   NEXT\n");
+	fprintf(stderr, "===============FREE LIST==============\n");
+	fprintf(stderr, "NODE # |   ADDRESS |   SIZE |   NEXT\n");
 
 	do {
 		if(current == NULL) {
@@ -132,7 +161,7 @@ void print_free_list() {
 		addr = (long) current;
 		sz = current->size;
 		nx = (long) current->next;
-		printf("%7d|  0x%x|%8d|    0x%x\n", nd, addr, sz, nx);
+		fprintf(stderr, "%7d|  0x%x|%8d|    0x%x\n", nd, addr, sz, nx);
 
 		++counter;
 		if(current->next != NULL) {
@@ -143,5 +172,5 @@ void print_free_list() {
 		}
 
 	} while(1);
-	printf("======================================\n");
+	fprintf(stderr, "======================================\n");
 }
