003391350f1ae3ca6df35ce50eec696104fcfe93
Camron Fritz <camfritz@10-116-51-93.wireless.oregonstate.edu>
2018-02-05 13:11:44 -0800

    2-5-18 13:11

--- hw3/my_tar.c
+++ hw3/my_tar.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <unistd.h>
+#include <utime.h>
 
 int main(int argc, char **argv) {
 	struct stat fileInfo;
@@ -10,6 +12,7 @@ int main(int argc, char **argv) {
 	FILE* filep;
 	char *fileBuffer;
 	long fileSize;
+	//error check cmd line arguments
 	if(argc < 2) {
 		fprintf(stderr, "Usage: ./my_tar -c file1 [file2 ...] or ./my_tar -x\n");
 		exit(1);
@@ -32,175 +35,85 @@ int main(int argc, char **argv) {
 				fprintf(stderr, "Not a regular file, skipping...\n");
 				--fileArgumentCount;
 			}
+			if(access(argv[fileArgumentCount + 1], R_OK) != 0) {
+				fprintf(stderr, "File does not have read permissions..skipping\n");
+			}
 			else {
+			}
 			//print file name
-				fwrite(argv[fileArgumentCount + 1], strlen(argv[fileArgumentCount + 1]), 1, stdout);
-				fprintf(stdout, "%c", ' ');
+			fprintf(stdout, "%s\n", argv[fileArgumentCount + 1]);
+			fprintf(stdout, "%c", ' ');
 			//print file stats
-				fwrite(&fileInfo, sizeof(struct stat), 1, stdout);
-				fprintf(stdout, "%c", ' ');
+			fwrite(&fileInfo, sizeof(struct stat), 1, stdout);
 			//print file contents
-				if(!(filep = fopen(argv[fileArgumentCount + 1], "rb"))) {
-					perror("ERROR: ");
-					exit(1);
-				}
+			if(!(filep = fopen(argv[fileArgumentCount + 1], "rb"))) {
+				perror("ERROR: ");
+				exit(1);
+			}
 
 				//obtain size of file
-				fseek(filep, 0, SEEK_END);
-				fileSize = ftell(filep);
-				rewind(filep);
+			fseek(filep, 0, SEEK_END);
+			fileSize = ftell(filep);
+			rewind(filep);
 
 				//allocate memory for file
-				fileBuffer = (char *) malloc(fileSize * sizeof(char));
-				if(fileBuffer == NULL) {
-					perror("ERROR: ");
-					exit(1);
-				}
+			fileBuffer = (char *) malloc(fileSize * sizeof(char));
+			if(fileBuffer == NULL) {
+				perror("ERROR: ");
+				exit(1);
+			}
 
 				//copy file into buffer
-				size_t copyCheck = fread(fileBuffer, 1, fileSize, filep);
-				if(copyCheck != fileSize) {
-					perror("ERROR: ");
-					exit(1);
-				}
-
-				fwrite(fileBuffer, 1, fileSize, stdout);
-
-				fprintf(stdout, "%c", ' ');
-				--fileArgumentCount;
-				fclose(filep);
-				free(fileBuffer);
+			size_t copyCheck = fread(fileBuffer, 1, fileSize, filep);
+			if(copyCheck != fileSize) {
+				perror("ERROR: ");
+				exit(1);
 			}
+
+			fwrite(fileBuffer, 1, fileSize, stdout);
+
+			fprintf(stdout, "%c", ' ');
+			--fileArgumentCount;
+			fclose(filep);
+			free(fileBuffer);
 		}
 	}
 
 	else {
+		char fileName[1000];
+		char *fileContents;
 		struct stat temp;
-		int scanLoop = 3, readInCounter = 0;
-		char *readIn;
+		struct utimbuf changeTimes;
+		FILE* filep;
 
-		//obtain size of standard input
-		filep = freopen(NULL, "rb", stdin);
-		fseek(filep, 0, SEEK_END);
-		fileSize = ftell(filep);
-		rewind(filep);
-
-		//allocate memory for standard input
-		fileBuffer = (char *) malloc(fileSize * sizeof(char));
-		readIn = (char *) malloc(fileSize * sizeof(char));
-		if(fileBuffer == NULL) {
-			perror("ERROR: ");
-			exit(1);
-		}
-
-		//copy standard input into buffer
-		size_t copyCheck = fread(fileBuffer, 1, fileSize, filep);
-		if(copyCheck != fileSize) {
-			perror("ERROR: ");
-			exit(1);
-		}
-		fclose(filep);
-
-		//iterate through buffer
-
-		//get name, create file
-		int i;
-		for(i = 0; i < fileSize; i++) {
-			if(fileBuffer[i] != ' ') {
-				readIn[readInCounter] = fileBuffer[i];
-				++readInCounter;
+		//while file names still to be read, create file with file name, readin stats, write contents, change modes and access/mod times
+		while(scanf("%s ", fileName) > 0) {
+			if(!(filep = fopen(fileName, "w+b"))) {
+				perror("ERROR: ");
+				exit(1);
 			}
-			else {
-				if(readIn[0] == '\0') {
-					break;
-				}
+			printf("%s\n", fileName);
+			fread(&temp, sizeof(struct stat), 1, stdin);
+			printf("THE SIZE OF THE FILE IS: %lld\n", temp.st_size);
 
-				if(!(filep = fopen(readIn, "w+b"))) {
-					perror("ERROR: ");
-					exit(1);
-				}
-				readInCounter = 0;
-				readIn[readInCounter] = '\0';
-
-				for(int j = 0; j < sizeof(struct stat); j++) {
-					++i;
-					readIn[j] = fileBuffer[i];
-				}
-				memcpy(&temp, readIn, sizeof(struct stat));
-				readInCounter = 0;
-				readIn[readInCounter] = '\0';
-
-				++i;
-				for(int j = 0; j < temp.st_size; j++) {
-					++i;
-					readIn[j] = fileBuffer[i];
-					// fprintf(stderr, "%c\n", fileBuffer[j]);
-				}
-				fwrite(readIn, temp.st_size - 1, 1, filep);
-				readInCounter = 0;
-				readIn[readInCounter] = '\0';
-
-				fclose(filep);
-				//++i;
-				// if((fileSize - i) < sizeof(struct stat)) {
-				// 	break;
-				// }
-
-				//fprintf(stderr, "%lld\n", temp.st_size);
-				//fprintf(stderr, "%s\n", readIn);
-
-					// case 2:
-					// fread(&temp, sizeof(struct stat), 1, readIn);
-					// readInCounter = 0;
-					// readIn[readInCounter] = '\0';
-					// --scanLoop;
-					// break;
-
-					// case 1:
-					// fwrite(readIn, temp.st_size, 1, filep);
-					// readInCounter = 0;
-					// readIn[readInCounter] = '\0';
-					// --scanLoop;
-					// break;
+			fileContents = (char *) malloc(temp.st_size * sizeof(char));
+			fread(fileContents, temp.st_size, 1, stdin);
+			fwrite(fileContents, temp.st_size, 1, filep);
+			fclose(filep);
+			if(chmod(fileName, temp.st_mode) != 0) {
+				perror("ERROR: ");
+				exit(1);
 			}
 
+			changeTimes.actime = temp.st_atime;
+			changeTimes.modtime = temp.st_mtime;
+			if(utime(fileName, &changeTimes) != 0) {
+				perror("ERROR: ");
+				exit(1);
+			}
+			free(fileContents);
 		}
-		//fwrite(fileBuffer, 1, fileSize, stdout);
-		free(fileBuffer);
+		//free(fileContents);
 	}
-
 
 }
-
-/*test code
-fread(&temp, sizeof(struct stat), 1, stdin);
-*/
-
-/*
-	else {
-		struct stat temp;
-		scanLoop = 3;
-		while(scanf("%s", str) != EOF) {
-			switch(scanLoop) {
-				case 3:
-				filep = fopen(str, "w+b");
-				//while ((getchar()) != '\n');
-				--scanLoop;
-				break;
-
-				case 2:
-				fread(&temp, sizeof(struct stat), 1, stdin);
-				// fread(&fileInfo, sizeof(struct stat), 1, stdin);
-				printf("******FILE SIZE IS******: %lld\n", temp.st_size);
-				fwrite(&temp, sizeof(struct stat), 1, stdout);
-				--scanLoop;
-				break;
-
-				case 1:
-				//fwrite(stdin, strlen(str), 1, stdout);
-				break;
-			}
-		}
-		fclose(filep);
-	}
-	*/
